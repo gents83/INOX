3ac3e0e90989a68d9409d2a1433315bcf4d22a3c
Mauro Gentile <gents83@gmail.com>
2021-04-07 20:27:02 +0200

    Removing useless meshdata generated per-frame

--- gui/src/widgets/editable_text.rs
+++ gui/src/widgets/editable_text.rs
@@ -1,5 +1,5 @@
 use nrg_commands::ExecuteCommand;
-use nrg_graphics::{MeshData, Renderer};
+use nrg_graphics::Renderer;
 use nrg_math::Vector4u;
 use nrg_platform::{
     EventsRw, InputHandler, InputState, Key, KeyEvent, KeyTextEvent, MouseButton, MouseEvent,
@@ -260,16 +260,6 @@ impl InternalWidget for EditableText {
     ) {
         self.check_focus(events_rw);
         self.update_text(events_rw);
-
-        let data = self.get_data_mut();
-        let pos = Screen::convert_from_pixels_into_screen_space(data.state.get_position());
-        let size = Screen::convert_size_from_pixels(data.state.get_size());
-        let mut mesh_data = MeshData::default();
-        mesh_data
-            .add_quad_default([0.0, 0.0, size.x, size.y].into(), data.state.get_layer())
-            .set_vertex_color(data.graphics.get_color());
-        mesh_data.translate([pos.x, pos.y, 0.0].into());
-        data.graphics.set_mesh_data(mesh_data);
     }
 
     fn widget_uninit(&mut self, _renderer: &mut Renderer) {}
