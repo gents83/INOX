e0ffa0aecc3162cfba608d924de4ddc439eab106
Mauro Gentile <gents83@gmail.com>
2022-02-28 08:21:19 +0100

    Removing useless code

--- crates/graphics/src/platform/wasm.rs
+++ crates/graphics/src/platform/wasm.rs
@@ -1,11 +1,7 @@
 #![cfg(target_arch = "wasm32")]
 
 pub fn required_gpu_features() -> wgpu::Features {
-    wgpu::Features::default()
-        | wgpu::Features::POLYGON_MODE_LINE
-        | wgpu::Features::INDIRECT_FIRST_INSTANCE
-        | wgpu::Features::MULTI_DRAW_INDIRECT
-        | wgpu::Features::MULTI_DRAW_INDIRECT_COUNT
+    wgpu::Features::default() | wgpu::Features::POLYGON_MODE_LINE
 }
 
 pub fn is_indirect_mode_enabled() -> bool {
