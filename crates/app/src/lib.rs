3edcf5a5542024ab5a3c8974b6c50e48963466fb
gents83 <gents83@gmail.com>
2023-10-22 11:21:27 +0200

    moving main into lib

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -3,3 +3,19 @@
 
 pub mod launcher;
 pub mod platform;
+
+
+pub fn main() {
+    use std::sync::Arc;
+    use crate::{launcher::Launcher, platform::*};
+
+    setup_env();
+
+    let launcher = Arc::new(Launcher::default());
+
+    load_plugins(&launcher);
+
+    launcher.start();
+
+    main_update(launcher);
+}
