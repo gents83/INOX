76d46ce60b1f80d46c9093ec83bdeb32e2f48ba3
gents83 <gents83@users.noreply.github.com>
2024-01-11 09:27:29 +0000

    deploy: 7680ce9e890eeeef8da42f14373a2ef1b5c12717

--- docs/web/inox_launcher.js
+++ docs/web/inox_launcher.js
@@ -1166,15 +1166,15 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 32, __wbg_adapter_33);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper7642 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper7643 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 32, __wbg_adapter_33);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8825 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper8826 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 32, __wbg_adapter_33);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8831 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper8832 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 32, __wbg_adapter_33);
         return addHeapObject(ret);
     };
